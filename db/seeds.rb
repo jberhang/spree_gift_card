a365d664f29ebe67edaa8593604eef060471ab41
Christopher Knight <christopher@scratch.com>
2016-12-05 11:11:33 -0500

    Update seeds.rb
    newe prod suite prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ if Spree::Product.gift_cards.count == 0
   puts "\tCreating default gift card..."
   option_type = Spree::OptionType.new(name: "is-gift-card", presentation: "Value")
   product.option_types << option_type
-  [100,125,150,175,200,250,325,375,425,450,495,550,600,725,1000,2250,2550,5550].each do |value|
+  [100,125,150,175,200,250,300,350,400,450,499,550,600,750,1000,2000,4999,6499].each do |value|
     option_value = Spree::OptionValue.new(name: value, presentation: "$#{value}")
     option_value.option_type = option_type
     opts = { price: value.to_i, sku: "GiftCard-#{value}" }
